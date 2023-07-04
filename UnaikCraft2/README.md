140ac6045a6536e49d11efcd5b238ce003f92feb
Ulfiyah <ulfiyahazura499@gmail.com>
2023-07-04 11:48:27 +0700

    update2

--- UnaikCraft2/README.md
+++
@@ -1,2 +0,0 @@
-# UnaikCraft
- Tugas CRUD Part 1
--- UnaikCraft2/app/src/main/java/com/example/unaikcraft/application/UnaikApp.kt
+++
@@ -1,9 +0,0 @@
-package com.example.unaikcraft.application
-
-import android.app.Application
-import com.example.unaikcraft.Repository.CraftRepository
-
-class UnaikApp : Application() {
-    val database by lazy { UnaikDatabase.getDatabase(this) }
-    val repository by lazy { CraftRepository(database.crafDao()) }
-}
